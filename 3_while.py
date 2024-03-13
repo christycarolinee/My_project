6c73a249b9b70fa1205f70dc82a6f357001e64b6
christycarolinee <christycarolinee30@gmail.com>
2024-03-13 11:04:50 +0800

    add

---
+++ 3_while.py
@@ -0,0 +1,23 @@
+#
+# While test
+# Christy
+#
+
+sum = 0
+
+while True :
+    # 1. Input 
+    x1 = input('Type x1: ')
+    x2 = input('Type x2: ')
+    op = input('Operator: ')
+
+    # 2. Process
+    if op == '+':
+        sum = int(x1) + int(x2) 
+    elif op == '-':
+        sum = int(x1) - int(x2) 
+
+    # 3. output 
+    print (f'Result: {sum}')
+
+    
