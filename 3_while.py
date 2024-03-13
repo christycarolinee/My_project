f8b1a2bc9bce274af2fa44bb10c29dae5e86b2d8
christycarolinee <christycarolinee30@gmail.com>
2024-03-13 11:59:26 +0800

    add

--- 3_while.py
+++ 3_while.py
@@ -12,16 +12,20 @@ while True:
     x1 = input('Type x1: ')
     x2 = input('Type x2: ')
     op = input('Operator: ')
-
-
+    
     # 2. Process
     if op == '+':
         sum = int(x1) + int(x2) 
     elif op == '-':
         sum = int(x1) - int(x2) 
-
-
+   
     # 3. Output 
     print (f'Result: {sum}')
 
 
+    option = input('Do you want to continue?')
+    if option == 'c' :
+                continue
+    else:
+          break 
+   
