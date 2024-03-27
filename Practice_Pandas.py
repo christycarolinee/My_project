9968bdda04a6e0eba1def8219689dd2a24018200
christycarolinee <christycarolinee30@gmail.com>
2024-03-27 11:00:56 +0800

    pandas

---
+++ Practice_Pandas.py
@@ -0,0 +1,23 @@
+#
+# Christy 
+# Practice Pandas Package 
+#
+
+import pandas as pd 
+
+my_data = pd.read_csv("Data.csv")
+
+print(my_data)
+print(my_data.info())
+
+# Dop mising data
+my_data = my_data.dropna()
+print(my_data.info())
+
+# Fix wrong format
+my_data['Date'] = pd.to_datetime(my_data['Date'],format='mixed')
+print(my_data)
+
+# fix typo
+my_data.loc[7,'Duration'] = 45
+print(my_data)
